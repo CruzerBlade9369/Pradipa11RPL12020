2d99a772dd5608cafc1791de38aa9e27f6d3f27f
Pradipa Rasendrya <pradiparasendrya@gmail.com>
2020-09-05 22:47:10 +0700

    json parsing unt pts

--- app/src/main/java/com/example/pradipa_11rpl1_2020/ListData.java
+++ app/src/main/java/com/example/pradipa_11rpl1_2020/ListData.java
@@ -5,7 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -21,7 +31,8 @@ public class ListData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_data);
         recyclerView = (RecyclerView) findViewById(R.id.rvdata);
-        addData();
+        //addData();
+        addDataOnline();
     }
 
     void addData() {
@@ -56,4 +67,66 @@ public class ListData extends AppCompatActivity {
 
 
     }
+
+    void addDataOnline(){
+        AndroidNetworking.get("https://api.themoviedb.org/3/movie/now_playing?api_key=7f1ea50a0eefdeb963193556a0423cda&language=en-US")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        Log.d("hasiljson", "onResponse: " + response.toString());
+                        //jika sudah berhasil debugm lanjutkan code dibawah ini
+
+                        DataArrayList = new ArrayList<>();
+                        Model MyModel;
+                        try {
+                            Log.d("hasiljson", "onResponse: " + response.toString());
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            Log.d("hasiljson2", "onResponse: " + jsonArray.toString());
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                MyModel = new Model();
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                MyModel.setOriginal_title(jsonObject.getString("original_title"));
+                                MyModel.setOverview(jsonObject.getString("overview"));
+                                MyModel.setRelease_date(jsonObject.getString("release_date"));
+                                MyModel.setPoster_path("https://image.tmdb.org/t/p/w500/"+jsonObject.getString("poster_path"));
+                                MyModel.setAdult(jsonObject.getBoolean("adult"));
+                                MyModel.setVote_count(jsonObject.getInt("vote_count"));
+                                MyModel.setOverview(jsonObject.getString("overview"));
+
+                                DataArrayList.add(MyModel);
+                            }
+
+                            adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+                                @Override
+                                public void onClick(int position) {
+
+                                }
+
+                                @Override
+                                public void test() {
+
+                                }
+                            });
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
+                            recyclerView.setLayoutManager(layoutManager);
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("errorku", "onError errorCode : " + error.getErrorCode());
+                        Log.d("errorku", "onError errorBody : " + error.getErrorBody());
+                        Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
+                    }
+                });
+        }
 }
